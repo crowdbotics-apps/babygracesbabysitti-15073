aae7b04f67504344da296097b6b32224fbec533f
Crowdbotics <team@crowdbotics.com>
2020-03-26 00:10:20 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/babygracesbabysitti_15073/MainActivity.java
@@ -0,0 +1,15 @@
+package com.babygracesbabysitti_15073;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "babygracesbabysitti_15073";
+    }
+}
